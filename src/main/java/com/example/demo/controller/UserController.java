4d4f80e354b10afadf2b3ab288179754fd7d2fb2
singhjaijeet106 <singhjaijeet106@gmail.com>
2024-01-08 19:11:19 -0500

    added controller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.model.UserDetail;
+
 @RestController
 @RequestMapping(path = "/api/user")
 public class UserController {
@@ -18,4 +20,7 @@ public class UserController {
 		return "please visit us again soon";
 	}
 
+	public UserDetail getUserDetails() {
+		return null;
+	}
 }
