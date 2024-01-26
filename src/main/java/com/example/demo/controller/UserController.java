fb4af3aaebc0ecb3c8ab3f7b90fedd8379a9334f
singhjaijeet106 <singhjaijeet106@gmail.com>
2024-01-26 13:30:27 -0500

    update the method type

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.UserDetail;
@@ -10,12 +11,12 @@ import com.example.demo.model.UserDetail;
 @RequestMapping(path = "/api/user")
 public class UserController {
 
-	@GetMapping(path = "/welcome")	
+	@RequestMapping(path = "/welcome", method = RequestMethod.GET)	
 	public String welcome() {
 		return "welcome to the new api endpoint";
 	}
 	
-	@GetMapping(path = "/exit")	
+	@RequestMapping(path = "/exit", method = RequestMethod.GET)	
 	public String exit() {
 		return "please visit us again soon";
 	}
